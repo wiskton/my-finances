76907454a9830a0c6d85ea4cfbe2e7110d1435b6
Willem Allan <willemarf@outlook.com>
2022-10-17 00:22:33 -0300

    update auth

--- backend-node/src/config/database.js
+++ backend-node/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.createConnection('mongodb://localhost/mymoney')
+module.exports = mongoose.connect('mongodb://localhost/db_finance', { useMongoClient: true })
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
 mongoose.Error.messages.Number.min = 
